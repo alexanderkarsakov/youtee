2c0fd9bd95a4f771e9c212600de179cd88b0dcf4
Aleksandr Karsakov <40732933+alexanderkarsakov@users.noreply.github.com>
2023-04-23 02:15:11 +0700

    Add files via upload

---
+++ src/utils/constants.js
@@ -0,0 +1,40 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import ArticleIcon from '@mui/icons-material/Article';
+import RecyclingIcon from '@mui/icons-material/Recycling';
+import DiamondIcon from '@mui/icons-material/Diamond';
+import SportsHandballIcon from '@mui/icons-material/SportsHandball';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
+import SchoolIcon from '@mui/icons-material/School';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
+import DirectionsCarFilledIcon from '@mui/icons-material/DirectionsCarFilled';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+  { name: 'Finance', icon: <AttachMoneyIcon />, },
+  { name: 'Sustainability', icon: <RecyclingIcon />, },
+  { name: 'Business', icon: <BusinessCenterIcon />, },
+  { name: 'Trade', icon: <CurrencyExchangeIcon />, },
+  { name: 'News', icon: <ArticleIcon />, },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'Health', icon: <HealthAndSafetyIcon />, },
+  { name: 'Sport', icon: <SportsHandballIcon />, },
+  { name: 'Beauty', icon: <DiamondIcon />, },
+  { name: 'Fashion', icon: <CheckroomIcon />, },
+  { name: 'eSports', icon: <SportsEsportsIcon />, },
+  { name: 'Cars', icon: <DirectionsCarFilledIcon />, },
+  
+];
+
+export const logo = 'https://i.ibb.co/YfqhwL9/logo.png';
+export const demoThumbnailUrl = 'https://i.ibb.co/FhcMB38/pic.png';
+export const demoChannelUrl = '/channel/@SpaceX';
+export const demoVideoUrl = '/video/921VbEMAwwY';
+export const demoChannelTitle = 'SpaceX';
+export const demoVideoTitle = 'Mars Mission';
+export const demoProfilePicture = 'https://i.ibb.co/FhcMB38/pic.png'
